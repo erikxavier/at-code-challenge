23561f84591424c270ccd22b9a0962dd22298234
Erik Xavier <erik.xam@gmail.com>
2019-05-29 13:53:24 -0300

    Adds navbard and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootswatch/dist/lux/bootstrap.min.css'
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App'>
+      <nav class='navbar navbar-expand-lg navbar-dark bg-primary'>
+        <a class='navbar-brand' href='#'>
+          <i class='fas fa-chess-rook' /> Movie Castle
         </a>
-      </header>
+        <form class='form-inline my-2 my-lg-0 nav navbar-nav ml-auto w-100 justify-content-end'>
+          <input class='form-control form-control-sm mr-sm-2' type='search' placeholder='Search movie' aria-label='Search' />
+          <button class='btn btn-sm btn-xs-block btn-outline-info mt-2 my-md-0 d-inline' type='submit'>Search</button>
+        </form>
+      </nav>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
