fa20ae2897c5fe1232606267cd8db64b3946c47c
Erik Xavier <erik.xam@gmail.com>
2019-05-31 13:39:08 -0300

    Adds a spinner to movie-card loading img

--- client/src/components/movie-card.js
+++ client/src/components/movie-card.js
@@ -10,13 +10,14 @@ export default (props) => {
     ...Object.keys(movie.backdrop).map(size => movie.backdrop[size]).reverse(),
     noPoster
   ]
+  const imgLoader = <div className='col-md-3 col-12 d-flex justify-content-center align-items-center'><div className='spinner-border center' role='status' /></div>
   return (
     <div className='p-3 mt-auto col-md-3 col-12'>
       <h5 className='align-center'>{movie.title}</h5>
       <h6>Release: {new Date(movie.release).toDateString()}</h6>
       <i>Genre: {movie.genres.slice(0, 2).join(', ')}</i>
       <Link to={`/movie/${movie.id}`} >
-        <Img className='my-2' src={[movie.poster['w500'], ...fallbackImages]} alt={`${movie.title} poster`} style={{ maxWidth: '100%' }} />
+        <Img className='my-2' src={[movie.poster['w500'], ...fallbackImages]} loader={imgLoader} alt={`${movie.title} poster`} style={{ maxWidth: '100%' }} />
         <p className='button button-link'> View Details...</p>
       </Link>
     </div>
