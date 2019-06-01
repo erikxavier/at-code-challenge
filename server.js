d33f9fa33f75caac5f60a863db2b759302535c51
Erik Xavier <erik.xam@gmail.com>
2019-05-31 23:39:13 -0300

    Fixes server PORT typo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const PORT = process.env.PORT | 4000
+const PORT = process.env.PORT || 4000
 const movieRoutes = require('./src/routes/movie-routes')
 const path = require('path')
 const { fetchApiConfigurations } = require('./src/data/TMDApi')
